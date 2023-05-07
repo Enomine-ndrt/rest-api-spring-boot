017c744baba75efdeadbb4d355dfa37aeb1b7feb
Luis Angel de los Santos Jimenez <ldelossantos@geovirtual.mx>
2023-05-07 15:11:34 -0600

    first commit

---
+++ src/main/java/com/agenciacristal/crud/product/ProductService.java
@@ -0,0 +1,75 @@
+package com.agenciacristal.crud.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    HashMap<String,Object> datos;
+
+    private final ProductRepository productRepository;
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+    @GetMapping
+    public List<Product> getProducts(){
+        return this.productRepository.findAll();
+    }
+
+    public ResponseEntity<Object> newProduct(Product product) {
+        Optional<Product> res = productRepository.findProductByName(product.getName());
+        datos = new HashMap<>();
+
+        if(res.isPresent() && product.getId() == null){
+            datos.put("error",true);
+            datos.put("message","Ya existe un producto con ese nombre");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.CONFLICT
+            );
+        }
+
+        datos.put("message","Se guardo con exito");
+        if(product.getId() != null){
+            datos.put("message","Se actualizo con exito");
+        }
+
+        productRepository.save(product);
+        datos.put("data",product);
+
+        return new ResponseEntity<>(
+                datos,
+                HttpStatus.CREATED
+        );
+    }
+
+    public ResponseEntity<Object> deleteProduct(Long id){
+      boolean existe = this.productRepository.existsById(id);
+        datos = new HashMap<>();
+
+      if(!existe){
+          datos.put("error",true);
+          datos.put("message","No existe un producto con ese id");
+          return new ResponseEntity<>(
+                  datos,
+                  HttpStatus.CONFLICT
+          );
+      }
+
+        productRepository.deleteById(id);
+        datos.put("message","Producto eliminado");
+        return new ResponseEntity<>(
+                datos,
+                HttpStatus.ACCEPTED
+        );
+
+    }
+}
